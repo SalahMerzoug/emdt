726e029d968e95a9f3bbeaefd0ed97a8a54efc79
mathieu.ruaud <mathieu.ruaud@ccb5b164-bc53-11dd-94c0-21daf384169b>
2008-12-03 15:00:04 +0000

    Modifications erreurs sur : -Planning.cs -Semaine.cs

--- trunk/PlanningMaker/Modele/Planning.cs
+++ trunk/PlanningMaker/Modele/Planning.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.ObjectModel;
 
 namespace PlanningMaker.Modele
 {
@@ -10,6 +11,9 @@ namespace PlanningMaker.Modele
         private EAnnees annee;
         private EDivisions division;
         private String promotion;
+
+        private ObservableCollection<Enseignant> enseignants;
+
 
         public EAnnees Annee
         {
