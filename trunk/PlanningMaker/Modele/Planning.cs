534d7d85dce6e25e18cfc6350f6f82fe4bd8e276
hdefleurian <hdefleurian@ccb5b164-bc53-11dd-94c0-21daf384169b>
2008-12-03 16:12:19 +0000

    Ajout des prototypes des fonctions de sauvegarde et chargement d'un fichier XML dans Planning.cs

--- trunk/PlanningMaker/Modele/Planning.cs
+++ trunk/PlanningMaker/Modele/Planning.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections.ObjectModel;
+using System.Xml;
 
 namespace PlanningMaker.Modele
 {
@@ -116,6 +117,87 @@ namespace PlanningMaker.Modele
             horaires = new ObservableCollection<Horaire>();
             salles = new ObservableCollection<Salle>();
             semaines = new ObservableCollection<Semaine>();
+        }
+
+        public void Charger(string nomFichier)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(nomFichier);
+            XmlNode elementPlanning = document.SelectSingleNode("/emploi-du-temps");
+            string valueAnnee = elementPlanning.SelectSingleNode("annee/text()").Value;
+            switch (valueAnnee)
+            {
+                case "P1" :
+                    annee = EAnnees.P1;
+                    break;
+                case "P2" :
+                    annee = EAnnees.P2;
+                    break;
+                case "I1" :
+                    annee = EAnnees.I1;
+                    break;
+                case "I2" :
+                    annee = EAnnees.I2;
+                    break;
+                case "I3" :
+                    annee = EAnnees.I3;
+                    break;
+            }
+            string valueDivision = elementPlanning.SelectSingleNode("division/text()").Value;
+            switch (valueDivision)
+            {
+                case "A":
+                    division = EDivisions.A;
+                    break;
+                case "B":
+                    division = EDivisions.B;
+                    break;
+                case "C":
+                    division = EDivisions.C;
+                    break;
+                case "D":
+                    division = EDivisions.D;
+                    break;
+            }
+            promotion = elementPlanning.SelectSingleNode("promotion/text()").Value;
+            //enseignants
+            Dictionary<string, Enseignant> dicoEnseignants = new Dictionary<string, Enseignant>();
+            foreach (XmlNode elementHoraire in document.SelectNodes("/emploi-du-temps/enseignants/enseignant"))
+            {
+
+            }
+            //matieres
+            Dictionary<string, Matiere> dicoMatieres = new Dictionary<string, Matiere>();
+            foreach (XmlNode elementHoraire in document.SelectNodes("/emploi-du-temps/matieres/matiere"))
+            {
+
+            }
+            //horaires
+            Dictionary<string, Horaire> dicoHoraires = new Dictionary<string, Horaire>();
+            foreach (XmlNode elementHoraire in document.SelectNodes("/emploi-du-temps/horaires/horaire"))
+            {
+
+            }
+            //salles
+            Dictionary<string, Salle> dicoSalles = new Dictionary<string, Salle>();
+            foreach (XmlNode elementHoraire in document.SelectNodes("/emploi-du-temps/salles/salle"))
+            {
+
+            }
+            //semaines
+            foreach (XmlNode elementHoraire in document.SelectNodes("/emploi-du-temps/horaires/horaire"))
+            {
+
+            }
+        }
+
+        public void Sauver(string nomFichier)
+        {
+            XmlDocument document = new XmlDocument();
+            XmlDeclaration declaration = document.CreateXmlDeclaration("1.0", "ISO-8859-15", null);
+            document.AppendChild(declaration);
+            // TODO
+            document.Save(nomFichier);
         }
     }
 }
