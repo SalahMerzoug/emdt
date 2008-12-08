db636dff9a7e3fb43cf1f42553755eb6a2c42e50
fr.alexisthomas <fr.alexisthomas@ccb5b164-bc53-11dd-94c0-21daf384169b>
2008-12-08 22:23:13 +0000

    Début de processeur XSLT - en attente de tests avec des XSLT réels.

---
+++ PlanningMaker/Modele/TransformationXslt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Xml.Xsl;
+
+namespace PlanningMaker.Modele
+{
+    class TransformationXslt
+    {
+
+        public void TransformerXslt(string nomFichierXSL, string nomFichierXML)
+        {
+            // Load the style sheet.
+            //XslCompiledTransform xslt = new XslCompiledTransform();
+            //xslt.Load(nomFichierXSL);
+
+            String nomFichierXMLsansExtension = nomFichierXML.Substring(0, nomFichierXML.LastIndexOf(".xml"));
+            MessageBox.Show(nomFichierXMLsansExtension);
+
+            // Execute the transform and output the results to a file.
+            //xslt.Transform(nomFichierXML, nomFichierXMLsansExtension + ".html");
+        }
+    }
+}
