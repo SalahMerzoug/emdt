2b44c658ec6bc08a9a82d785db40a2eed74a5fe6
fr.alexisthomas <fr.alexisthomas@ccb5b164-bc53-11dd-94c0-21daf384169b>
2008-12-10 15:19:11 +0000

    Supprimer élément.

--- PlanningMaker/Vues/PlanningCommands.cs
+++ PlanningMaker/Vues/PlanningCommands.cs
@@ -10,6 +10,7 @@ namespace PlanningMaker.Vues
 
         private static System.Windows.Input.RoutedUICommand exit;
         private static System.Windows.Input.RoutedUICommand ajouterElement;
+        private static System.Windows.Input.RoutedUICommand supprimerElement;
 
         static PlanningCommands()
         {
@@ -17,6 +18,8 @@ namespace PlanningMaker.Vues
                 "Exit", "Quitter PlanningMaker 2008", typeof(PlanningCommands));
             ajouterElement = new System.Windows.Input.RoutedUICommand(
                 "Ajouter", "Ajouter une ressource", typeof(PlanningCommands));
+            supprimerElement = new System.Windows.Input.RoutedUICommand(
+                "Supprimer", "Supprimer une ressource", typeof(PlanningCommands));
         }
 
         public static System.Windows.Input.RoutedUICommand Exit
@@ -32,6 +35,14 @@ namespace PlanningMaker.Vues
             get
             {
                 return ajouterElement;
+            }
+        }
+
+        public static System.Windows.Input.RoutedUICommand SupprimerElement
+        {
+            get
+            {
+                return supprimerElement;
             }
         }
     }
