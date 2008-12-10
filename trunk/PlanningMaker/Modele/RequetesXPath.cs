3fdb3f3c1781f462f41ceac185f68ee8a52f2084
denouille.freddy <denouille.freddy@ccb5b164-bc53-11dd-94c0-21daf384169b>
2008-12-10 15:20:41 +0000

    VueRequetesXPath  v1

--- trunk/PlanningMaker/Modele/RequetesXPath.cs
+++ trunk/PlanningMaker/Modele/RequetesXPath.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using System.Windows;
+using System.Xml;
+using System.Xml.XPath;
 using System.Xml.Xsl;
+
 
 namespace PlanningMaker.Modele
 {
@@ -8,14 +11,44 @@ namespace PlanningMaker.Modele
 
         public void ExecRequetesXPath(string nomFichierXSL, string nomFichierXML)
         {
+
             // Load the style sheet.
             XslCompiledTransform xslt = new XslCompiledTransform();
             xslt.Load(@"..\..\Files\" + nomFichierXSL);
+          
+            // Create the XsltArgumentList.
+            string numSemaine = "37";
+            string nom_recherche_1 = "oug";
+            string id_enseignant_2 = "obeaudoux";
+            string id_matière_3 = "maths";
+            string id_matière_4 = "anglais";
+            string id_enseignant_5 = "kdrouet";
+            string id_salle_6 = "langevin";
+            string id_jour_6 = "mardi";
+            string id_enseignant_7 = "kdrouet";
+            string id_jour_7 = "mardi";
 
-            String nomFichierXMLsansExtension = nomFichierXML.Substring(0, nomFichierXML.LastIndexOf(".xml"));
+            XsltArgumentList xslArg = new XsltArgumentList();
+            xslArg.AddParam("numSemaine", "", numSemaine);
+            xslArg.AddParam("nom_recherche_1", "", nom_recherche_1);
+            xslArg.AddParam("id_enseignant_2", "", id_enseignant_2);
+            xslArg.AddParam("id_matière_3", "", id_matière_3);
+            xslArg.AddParam("id_matière_4", "", id_matière_4);
+            xslArg.AddParam("id_enseignant_5", "", id_enseignant_5);
+            xslArg.AddParam("id_salle_6", "", id_salle_6);
+            xslArg.AddParam("id_jour_6", "", id_jour_6);
+            xslArg.AddParam("id_enseignant_7", "", id_enseignant_7);
+            xslArg.AddParam("id_jour_7", "", id_jour_7);
 
-            // Execute the transform and output the results to a file.
-            xslt.Transform(@"..\..\Files\" + nomFichierXML, @"..\..\Files\" + nomFichierXMLsansExtension + "-RequêtesXPath.html");
-        }
+            string nomFichierXMLsansExtension = nomFichierXML.Substring(0, nomFichierXML.LastIndexOf(".xml"));
+            XPathDocument xpathdocument = new XPathDocument(@"..\..\Files\" + nomFichierXML);
+            XmlTextWriter writer = new XmlTextWriter(@"..\..\Files\" + nomFichierXMLsansExtension + "-RequêtesXPath.html", null);
+            
+            // Transform the file.
+            xslt.Transform(xpathdocument, xslArg, writer);
+            writer.Close();
+
+            MessageBox.Show("Génération des requêtes XPath : OK");
+       }
     }
 }
---
+++ trunk/PlanningMaker/Vues/VueRequetesXPath.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PlanningMaker.Modele;
+
+namespace PlanningMaker.Vues
+{
+	/// <summary>
+	/// Interaction logic for VueHoraire.xaml
+	/// </summary>
+	public partial class VueRequetesXPath : UserControl
+	{
+
+
+		public VueRequetesXPath()
+		{
+            InitializeComponent();
+
+		}
+	}
+}
