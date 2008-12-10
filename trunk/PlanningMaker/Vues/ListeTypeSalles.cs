b6f9408188a13a7cc018b002f1370b8193a880d2
mathieu.ruaud <mathieu.ruaud@ccb5b164-bc53-11dd-94c0-21daf384169b>
2008-12-10 16:43:44 +0000

    Commit fin de scéance du 10/12/2008

---
+++ trunk/PlanningMaker/Vues/ListeTypeSalles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using PlanningMaker.Modele;
+
+namespace PlanningMaker.Vues
+{
+    public class ListeTypeSalles : ObservableCollection<string>
+    {
+        public ListeTypeSalles(){
+            foreach(String s in Enum.GetNames(typeof(ETypeSalles)))
+                Add(s);
+        }
+    }
+}
