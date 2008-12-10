99cc6e2479ce287961a5d81977aabe15ffa7da2c
mathieu.deniaud <mathieu.deniaud@ccb5b164-bc53-11dd-94c0-21daf384169b>
2008-12-10 15:19:19 +0000

    Binding enseigant

--- trunk/PlanningMaker/Vues/VueEnseignant.xaml.cs
+++ trunk/PlanningMaker/Vues/VueEnseignant.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PlanningMaker.Modele;
 
 namespace PlanningMaker.Vues
 {
@@ -19,9 +20,13 @@ namespace PlanningMaker.Vues
     /// </summary>
     public partial class VueEnseignant : UserControl
     {
+        private Enseignant enseigant;
+
         public VueEnseignant()
         {
             InitializeComponent();
+            enseigant = new Enseignant();
+            DataContext = enseigant;
         }
     }
 }
