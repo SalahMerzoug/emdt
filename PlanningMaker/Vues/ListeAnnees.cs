76d464ee8201e52a900325a48a8ea006ea95b21c
hdefleurian <hdefleurian@ccb5b164-bc53-11dd-94c0-21daf384169b>
2009-01-09 13:08:44 +0000

    Onglet Emploi-Du-Temps de MainWindow : Transformation du TextBox pour l'année en ComboBox Uniformisation graphique des tableaux des ressources

---
+++ PlanningMaker/Vues/ListeAnnees.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using PlanningMaker.Modele;
+
+namespace PlanningMaker.Vues
+{
+    public class ListeAnnees : ObservableCollection<string>
+    {
+        public ListeAnnees()
+        {
+            foreach (String s in Enum.GetNames(typeof(EAnnees)))
+                Add(s);
+        }
+    }
+}
