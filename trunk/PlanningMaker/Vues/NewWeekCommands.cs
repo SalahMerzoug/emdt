0e1590e8187a5af27017cab4eb4e9c1256691b18
mathieu.ruaud <mathieu.ruaud@ccb5b164-bc53-11dd-94c0-21daf384169b>
2009-01-09 14:57:12 +0000

    Ajout fichier NewWeekCommands.cs

---
+++ trunk/PlanningMaker/Vues/NewWeekCommands.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanningMaker.Vues
+{
+    public class NewWeekCommands
+    {
+        private static System.Windows.Input.RoutedUICommand ok;
+        private static System.Windows.Input.RoutedUICommand cancel;
+
+        static NewWeekCommands()
+        {
+            ok = new System.Windows.Input.RoutedUICommand(
+                "OK", "Créer une nouvelle semaine", typeof(NewWeekCommands));
+            cancel = new System.Windows.Input.RoutedUICommand(
+                "Cancel", "Annuler la création", typeof(NewWeekCommands));
+        }
+
+        public static System.Windows.Input.RoutedUICommand OK
+        {
+            get
+            {
+                return ok;
+            }
+        }
+
+        public static System.Windows.Input.RoutedUICommand Cancel
+        {
+            get
+            {
+                return cancel;
+            }
+        }
+    }
+}
