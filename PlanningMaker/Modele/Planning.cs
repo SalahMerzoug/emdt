228f854fd1bc079ce470369b13ce22957ccf52d3
mathieu.ruaud <mathieu.ruaud@ccb5b164-bc53-11dd-94c0-21daf384169b>
2009-01-22 07:16:14 +0000

    Pb lors de la séléction de l'enseignement réglé : ajout d'un booléen lors du rafraîchissement de la VueEnseignement.

--- PlanningMaker/Modele/Planning.cs
+++ PlanningMaker/Modele/Planning.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Xml;
+using System.Windows.Forms;
 
 namespace PlanningMaker.Modele
 {
